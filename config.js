11699b75d70a57bae996c7cc49e4d6b04875f1eb
Shaka56355 <shakadon416@gmail.com>
2024-11-12 02:34:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "nr53WazK#WhId7icyGVXdt2Hl0qxkE_EhJQ9fO-dpLVYTvRqKRcU",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_ADMIN: process.env.AUTO_ADMIN || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
