6986b026bcc65162155fc4950685742945a47971
Shaka56355 <shakadon416@gmail.com>
2024-11-13 01:45:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nr53WazK#WhId7icyGVXdt2Hl0qxkE_EhJQ9fO-dpLVYTvRqKRcU",
+SESSION_ID: process.env.SESSION_ID || "y3YQ1JpI#_rB7TPGE05LzC9wfRLqRqOM-7OAIvmSrbXePUvR2ZR8",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
